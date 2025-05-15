48e26c6616df9de1e09761f7eb08e1439cbca015
adelaserbulescu <163027170+adelaserbulescu@users.noreply.github.com>
2025-05-15 20:02:41 +0300

    Add files via upload

---
+++ Process_data.c
@@ -0,0 +1,19 @@
+#include "Talkthrough.h"
+
+//--------------------------------------------------------------------------//
+// Function:	Process_Data()												//
+//																			//
+// Description: This function is called from inside the SPORT0 ISR every 	//
+//				time a complete audio frame has been received. The new 		//
+//				input samples can be found in the variables iChannel0LeftIn,//
+//				iChannel0RightIn, iChannel1LeftIn and iChannel1RightIn 		//
+//				respectively. The processed	data should be stored in 		//
+//				iChannel0LeftOut, iChannel0RightOut, iChannel1LeftOut,		//
+//				iChannel1RightOut, iChannel2LeftOut and	iChannel2RightOut	//
+//				respectively.												//
+//--------------------------------------------------------------------------//
+void Process_Data(void)
+{
+	iChannel0LeftOut = ((iChannel0LeftIn<<8)>>3)>>8;
+	iChannel0RightOut = ((iChannel0RightIn<<8)>>0)>>8;
+}
